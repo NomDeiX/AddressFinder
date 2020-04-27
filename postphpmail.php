a9068a2b015a58a126fa44135e9eeb1e9477ca92
Samuel Vandak <48286115+NomDeiX@users.noreply.github.com>
2020-04-27 15:33:58 +0200

    Add files via upload

---
+++ postphpmail.php
@@ -0,0 +1,9 @@
+<?php
+
+$latitude = $_POST["latitude"];
+$longitude = $_POST["longitude"];
+
+echo $latitude
+
+mail("samuelvandak@gmail.com","You received someones geolocation! Hurray!","test");
+?>
